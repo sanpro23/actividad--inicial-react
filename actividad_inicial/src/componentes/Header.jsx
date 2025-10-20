051d33ce154a484f261fccc8061b87185f39d883
Antonio Santos <santosprogramacion23@hotmail.com>
2025-10-20 12:30:20 +0200

    septimo commit

--- actividad_inicial/src/componentes/Header.jsx
+++ actividad_inicial/src/componentes/Header.jsx
@@ -2,13 +2,38 @@
 
 function Header() {
     return (
-        <header className="header">
-            <div className="header-guitarras">
-                <h1>GUITARRAS Y ACCESORIOS DE SEGUNDAMANO</h1>
-            </div>
-            <img src="/cabecera.webp" alt="logo-header" />
-        </header >
+        <>
+            {/*Barra de navegacion superior*/}
+            <nav className="nav-menu">
+                <span class="menu-frase">LA SEXTA CUERDA</span>
+                <ul>
+                    <li><a href="#Inicio">Inicio</a></li>
+                    <li><a href="#Productos">Procuctos</a></li>
+                    <li><a href="#Peteciones">Peticiones</a></li>
+                    <li><a href="#Contacto">Contacto</a></li>
+                </ul>
+            </nav>
+
+            {/*Header principal con imagen de fondo*/}
+            <header className="header-modern">
+                <div className="header-background">
+                    <img src="/mastil3.webp" alt="fondo-header" />
+                    <div className="header-overlay"></div>  {/* Capa de superposición */}
+                </div>
+
+                {/* Contenido del header */}
+                <div className="header-content">
+                    <div className="header-title-wrapper">  {/* agrupa el titulo y linea decorativa*/}
+                        <h1>GUITARRAS Y ACCESORIOS DE SEGUNDAMANO</h1>
+                    </div>
+                </div>
+
+                {/*Degradado blanco */}
+                <div className="header-fade"></div>
+            </header >
+        </>
     );
 }
+
 
 export default Header 
